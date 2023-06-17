3c9f8c9963dcc1c3c0ba3322d76e3fb5c56e6d73
Faizan Qureshi <74370508+fqthedeveloper@users.noreply.github.com>
2023-06-17 15:04:24 +0530

    Delete forms.py

--- SKManagment/Main/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from . import models
-
-
-class AppointmentForm(forms.ModelForm):
-    class Meta:
-        model = models.Appointment
-        fields = ('full_name', 'email', 'phone_number', 'email_subject', 'Message')
